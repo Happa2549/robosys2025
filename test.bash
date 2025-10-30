b0c42e89b7a18d6c3db88f40d64d3a394b2d4cfc
happa <s24c1122vn@s.chibakoudai.jp>
2025-10-30 12:50:54 +0900

    Add code

--- test.bash
+++ test.bash
@@ -1,4 +1,6 @@
 #!/bin/bash
+#SPDX-FileCopyrightText: 2025 Morito Shunsuke
+
 
 ng () {
 	echo ${1}行目が違うよ
@@ -6,9 +8,11 @@ ng () {
 }
 
 res=0
-a=山田
-[ "$a" = 上田 ] || ng "$LINENO"
-[ "$a" = 山田 ] || ng "$LINENO"
+
+out=$(seq 5 | ./plus)
+[ "${out}" = 15.0 ] || ng "$LINENO"
+
+[ "${res}" = 0 ] && echo ok
 
 exit $res
 
