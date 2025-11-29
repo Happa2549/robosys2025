e440a7e55553b817f40114b978132efb818553f2
happa <s24c1122vn@s.chibakoudai.jp>
2025-11-30 06:49:41 +0900

    secondtest

--- test.bash
+++ test.bash
@@ -7,19 +7,14 @@ ng() {
 }
 
 res=0
-input_pdf="a.pdf"
 
 
-if [[ ! -f "$input_pdf" ]]; then
-    ng "$LINENO"
-fi
-
-
-echo "$input_pdf" | python3 cvt_pdf
+out=$(echo "a.pdf" | python3 cvt_pdf)
 if [[ $? -ne 0 ]]; then
     ng "$LINENO"
 fi
 
-[ "$res" = 0 ] && echo OK
-exit $res
 
+
+[ "${res}" = 0 ] && echo ok #for human 
+exit $res
